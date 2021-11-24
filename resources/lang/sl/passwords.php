1041bedca1ea6d7bbe8c9c643ba8984baef0508e
Sebestyén Németh <sebezhetetlen98@gmail.com>
2021-11-24 08:41:44 +0100

    New translations passwords.php (Slovenian)

--- resources/lang/sl/passwords.php
+++ resources/lang/sl/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Geslo mora imeti najmanj šest znakov in se mora ujemati s potrditvijo.',
-    'reset' => 'Tvoje geslo je bilo ponastavljeno!',
-    'sent' => 'Na tvoj email smo poslali povezavo za ponastavitev gesla!',
-    'token' => 'Ta ključ za ponastavitev gesla ni veljaven.',
-    'user' => "Ne najdemo uporabnika s tem e-naslovom.",
+    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that e-mail address.",
 
 ];
