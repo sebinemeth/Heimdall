f6b6f0ad3cd564b9caeec6019f7f7ff681acb9ea
Sebestyén Németh <sebezhetetlen98@gmail.com>
2021-11-24 08:40:53 +0100

    New translations auth.php (Slovenian)

--- resources/lang/sl/auth.php
+++ resources/lang/sl/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'Podatki za prijavo se ne ujemajo.',
-    'throttle' => 'Preveč poizkusov prijave. Ponovno poizkusi čez :seconds sekund.',
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
