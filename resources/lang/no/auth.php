9517b1e00047e36b01532fa7ea7b933513667ac5
Sebestyén Németh <sebezhetetlen98@gmail.com>
2021-11-24 08:40:51 +0100

    New translations auth.php (Norwegian)

--- resources/lang/no/auth.php
+++ resources/lang/no/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'Påloggingsinformasjonen stemmer ikke overens med våre data.',
-    'throttle' => 'For mange påloggingsforsøk. Prøv igjen om :seconds sekunder.',
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
