cfe08e5671ad8ca09e3184e0628597568f218a69
Sebestyén Németh <sebezhetetlen98@gmail.com>
2021-11-24 08:45:56 +0100

    New translations auth.php (Greek)

--- resources/lang/el/auth.php
+++ resources/lang/el/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Αυτά τα διαπιστευτήρια δεν ταιριάζουν με τα μητρώα μας.',
+    'throttle' => 'Πολλές προσπάθειες εισόδου. Παρακαλούμε προσπαθήστε ξανά σε :seconds δευτερόλεπτα.',
 
 ];
