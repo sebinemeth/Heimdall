70aca5876bbb5ebacf047f4842f89a900fc7de91
Sebestyén Németh <sebezhetetlen98@gmail.com>
2021-11-24 08:34:15 +0100

    New translations auth.php (Hungarian)

--- resources/lang/hu/auth.php
+++ resources/lang/hu/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'A belépési adatok nem találhatók.',
+    'throttle' => 'Túl sok bejelentkezési kísérlet. Próbáld újra :seconds mp múlva.',
 
 ];
