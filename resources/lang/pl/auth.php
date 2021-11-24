c713d2bdb224a64c0490e4981df5313f335d193d
Sebestyén Németh <sebezhetetlen98@gmail.com>
2021-11-24 08:45:30 +0100

    New translations auth.php (Polish)

--- resources/lang/pl/auth.php
+++ resources/lang/pl/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Nieprawidłowe dane uwierzytelnienia',
+    'throttle' => 'Zbyt wiele prób logowania. Spróbuj ponownie za :seconds sekund.',
 
 ];
