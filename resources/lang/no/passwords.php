d313778f0e693bfd3f0ec5251f7040b72f938785
Sebestyén Németh <sebezhetetlen98@gmail.com>
2021-11-24 08:41:47 +0100

    New translations passwords.php (Norwegian)

--- resources/lang/no/passwords.php
+++ resources/lang/no/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passord må være minst seks tegn og i samsvar med bekreftelsen.',
-    'reset' => 'Passordet ditt har blitt tilbakestilt!',
-    'sent' => 'Vi har sendt deg en e-post med en tilbakestillingslink for ditt passord!',
-    'token' => 'Denne tilgangstoken er ikke gyldig.',
-    'user' => "Vi fant ingen med den e-postadressen.",
+    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that e-mail address.",
 
 ];
